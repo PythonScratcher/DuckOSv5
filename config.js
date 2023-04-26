a9e96622e4996562b982cc21a3be4e57127de02d
ChainedTears <123793568+GikitSRC@users.noreply.github.com>
2023-04-25 19:32:09 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+const config = {
+  buttons: [
+    {
+      id: 'rammerhead-browser',
+      iframeSrc: 'https://everyone.isageek.net/',
+      defaultWidth: 800,
+      defaultHeight: 600,
+      buttonText: 'Rammerhead',
+      windowTitle: 'Rammerhead Browser',
+      imgSrc: '/system/media/rammerhead_icon.png'
+    },
+    {
+      id: 'meowzies-adventure',
+      iframeSrc: '/apps/meowiegames/meowziesadventurethree/index.html',
+      defaultWidth: 600,
+      defaultHeight: 400,
+      buttonText: 'Meowzies Adventure 3',
+      windowTitle: 'Meowzies Adventure 3',
+      imgSrc: '/apps/meowiegames/meowziesadventurethree/icon.png'
+    },
+    {
+      id: 'open-window-btn-2s',
+      iframeSrc: 'https://example.org',
+      defaultWidth: 600,
+      defaultHeight: 400,
+      buttonText: 'Open Window 2',
+      windowTitle: 'Window 2',
+      imgSrc: 'path/to/image3.png'
+    }
+  ]
+};
