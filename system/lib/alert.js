5a16125c84c1ac30630174639ea0c75b88916fea
ChainedTears <123793568+GikitSRC@users.noreply.github.com>
2023-04-27 21:13:41 -0700

    Update alert.js

--- system/lib/alert.js
+++ system/lib/alert.js
@@ -1,9 +1,20 @@
 window.addEventListener('load', function() {
     var loadingPage = document.querySelector('.loading-page');
-    setTimeout(function() {
-        loadingPage.classList.add('fade-out');
+    var delayTime = localStorage.getItem('delayTime');
+    if (delayTime === null) {
         setTimeout(function() {
-            loadingPage.style.display = 'none';
-        }, 500);
-    }, 3000);
+            loadingPage.classList.add('fade-out');
+            setTimeout(function() {
+                loadingPage.style.display = 'none';
+            }, 500);
+        }, 7000);
+        localStorage.setItem('delayTime', '7000');
+    } else {
+        setTimeout(function() {
+            loadingPage.classList.add('fade-out');
+            setTimeout(function() {
+                loadingPage.style.display = 'none';
+            }, 500);
+        }, 1);
+    }
 });
